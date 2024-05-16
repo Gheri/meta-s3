08a7ee1735222d2ee28ecfd82377a897c46f8741
Gheri <gheri.rupchandani@gmail.com>
2024-05-16 18:10:05 +0530

    Add health endpoint

--- routes/object.js
+++ routes/object.js
@@ -1,6 +1,8 @@
-const {putObject, getObject, deleteObject} = require("../controllers/object.js");
+const {putObject, getObject, deleteObject, healthObject} = require("../controllers/object.js");
 
 const routes = (app) => {
+    app.route('/health')
+        .get(healthObject);
     app.route('/data/:repositoryType')
         .put(putObject);
     app.route('/data/:repositoryType/:objectId')
